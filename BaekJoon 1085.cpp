24130e4dfca865da9f1777ab9fb736d0bed8089a
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-07-06 16:43:07 +0900

    Create BaekJoon 1085.cpp

---
+++ BaekJoon 1085.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int x, y, w, h;
+	int temp1, temp2, result;
+	cin >> x >> y >> w >> h;
+
+	temp1 = w - x;
+	temp2 = h - y;
+	result = x;
+	if (temp1 < result)
+	{
+		result = temp1;
+	}
+	if (y < result)
+	{
+		result = y;
+	}
+	if (temp2 < result)
+	{
+		result = temp2;
+	}
+	cout << result;
+}
+
