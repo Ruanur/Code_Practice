175290bb37f0c85695c9d6f7dd32af50ad0f76b5
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-07-09 16:30:53 +0900

    Create BaekJoon 2903.cpp

---
+++ BaekJoon 2903.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, result;
+	cin >> N;
+
+	int x = 2;
+
+	for (int i = 1; i <= N; i++)
+	{
+		x += (x - 1);
+	}
+	
+	result = x * x;
+	
+	cout << result << endl;
+}
