807970d08c145e5050a46a470b68f19dcff46bca
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-07-24 08:50:27 +0900

    Update BaekJoon 1284.cpp

--- BaekJoon 1284.cpp
+++ BaekJoon 1284.cpp
@@ -1,24 +1,35 @@
-#include <iostream>
-
+癤�#include <iostream>
 using namespace std;
-
-int main() {
-
+int main()
+{
 	string N;
-
-	while (1) {
+	while (true)
+	{
 		cin >> N;
-		if (N == "0") return 0;
-
-		int sum = 0;
-		for (int i = 0; i < N.length(); i++) {
-			if (N[i] == '1') sum += 2;
-			else if (N[i] == '0') sum += 4;
-			else sum += 3;
-			sum++; //각 숫자마다 여백 1cm
+		if (N == "0")
+		{
+			return 0;
 		}
-		cout << sum + 1 << "\n"; //가장 처음 여백 1cm 추가
-	}
+		int result = 0;
 
+		for (int i = 0; i < N.length(); i++)
+		{
+			if (N[i] == '1')
+			{
+				result += 2;
+			}
+			else if (N[i] == '0')
+			{
+				result += 4;
+			}
+			else
+			{
+				result += 3;
+			}
+			result++;
+		}
+		cout << result + 1 << endl;
+	}
 	return 0;
 }
+
