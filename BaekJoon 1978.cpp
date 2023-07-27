23fa68c6415458793bdda1b0269c3b499f9720ee
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-07-27 19:15:32 +0900

    Create BaekJoon 1978.cpp

---
+++ BaekJoon 1978.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, result = 0;
+	int p, stack = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p;
+		for (int a = 1; a <= p; a++)
+		{
+			if (p % a == 0)
+			{
+				stack++;
+			}
+		}
+		if (stack == 2)
+		{
+			result++;
+		}
+		stack = 0;
+	}
+	cout << result << endl;
+}
