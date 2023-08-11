76157f5b40291fb37d90d2db6ccc7d79a8c572ce
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-08-11 16:59:40 +0900

    Create BaekJoon 2869 Modify.cpp
    반복문 시간초과

---
+++ BaekJoon 2869 Modify.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B, V;
+	cin >> A >> B >> V;
+	int count = 1;
+
+	count += (V - A) / (A - B);
+	//EX. Day = 2 ,Night = 1, Length = 5
+	//(5-2) / (2-1) + 1 = "4" >> Count.
+	if (A >= V)
+	{
+		cout << "1";
+	}
+	if ((V - A) % (A - B) != 0)
+	{
+		count++;
+	}
+	//Ex. Day = 5, Night = 1, Length = 6
+	//1/4 + 1 = 1(False), if.
+	cout << count;
+}
