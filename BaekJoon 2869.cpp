a7b4b3a1df37c3f0d09c9256e924f732752ab257
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-08-11 16:40:41 +0900

    Create BaekJoon 2869.cpp

---
+++ BaekJoon 2869.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int climb(int a, int b, int c)
+{
+	int N = 0;
+	int temp = 0;
+	int count = 0;
+	while (N >= c)
+	{
+		N = N + a;
+		count++;
+		if (N < c)
+		{
+			N = N - b;
+		}
+	}
+	return count;
+}
+
+int main()
+{
+	int A, B, V;
+	cin >> A >> B >> V;
+	cout << climb(A, B, V);
+}
