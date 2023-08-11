c6d34b0ebe2b585d877eba56c2ad5b3f42d1725e
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-08-11 17:03:28 +0900

    Create BaekJoon 2869 Final.cpp

---
+++ BaekJoon 2869 Final.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B, V;
+	cin >> A >> B >> V;
+	int count = 1;
+
+	count += (V - A) / (A - B);
+	//EX. Day = 2 ,Night = 1, Length = 5
+	//(5-2) / (2-1) + 1 = "4" >> Count.
+	if ((V - A) % (A - B) != 0)
+	{
+		count++;
+	}
+	//Ex. Day = 5, Night = 1, Length = 6
+	//1/4 + 1 = 1(False), if.
+	if (A >= V)
+	{
+		cout << "1";
+	}
+	else
+	{
+		cout << count;
+	}
+	return 0;
+}
