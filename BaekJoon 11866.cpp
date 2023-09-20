142916de56a5baabf6c62ec449c09cee36a96541
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-09-20 19:24:16 +0900

    Create BaekJoon 11866.cpp
    Queue

---
+++ BaekJoon 11866.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int N, K;
+	cin >> N >> K;
+	queue<int> Q;
+
+	for (int i = 1; i <= N; i++)
+	{
+		Q.push(i); //큐에 수 집어넣기
+	}
+
+	cout << "<";
+
+	while (Q.size() > 1)
+	{
+		for (int i = 1; i < K; i++)
+		{
+			int first = Q.front();
+			Q.pop();
+			Q.push(first);
+		}
+		cout << Q.front() << ", ";
+		Q.pop();
+	}
+	cout << Q.front() << ">\n";
+}
