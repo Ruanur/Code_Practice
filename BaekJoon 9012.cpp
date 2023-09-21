530249d5fbb7488b14abc29be5a7331ef3fce354
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-09-21 13:53:56 +0900

    Create BaekJoon 9012.cpp
    Stack, String 라이브러리 응용 공부 필요

---
+++ BaekJoon 9012.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+	while (T > 0)
+	{
+		T--;
+		string V;
+		string answer = "YES";
+		cin >> V;
+		
+		stack<char> st;
+		for (int i = 0; i < V.length(); i++)
+		{
+			if (V[i] == '(')
+			{
+				st.push(V[i]);
+			}
+			else if (!st.empty() && st.top() == '(' && V[i] == ')')
+			{
+				st.pop();
+			}
+			else
+			{
+				answer = "NO";
+				break;
+			}
+		}
+		if (!st.empty())
+		{
+			answer = "NO";
+		}
+		cout << answer << '\n';
+	}
+	
+}
