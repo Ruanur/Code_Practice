da28bd79fdc1d70acee75c24467b1b508e1da0aa
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-09-24 12:47:22 +0900

    Create BaekJoon 2839.cpp
    5로 나눠서 떨어지면 답
    아니면 3으로 빼서 다시 나눔

---
+++ BaekJoon 2839.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int N;
+int main() {
+	cin >> N;
+	int result = 0;
+	while (N >= 0) 
+	{
+		if (N % 5 == 0) 
+		{	
+			result += (N / 5);	
+			cout << result << "\n";
+			return 0;
+		}
+		N -= 3;	
+		result++;
+	}
+	cout << -1 << "\n";
+}
