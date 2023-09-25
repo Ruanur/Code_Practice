f05758bff1162a287658feef5a058ebce7d33aeb
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-09-25 11:16:35 +0900

    Create BaekJoon 4949.cpp

---
+++ BaekJoon 4949.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N;
+	int input;
+	cin >> N;
+	int result = 0;
+	vector<int> arr;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> input;
+		if (input != 0)
+		{
+			arr.push_back(input);
+		}
+		else if (!arr.empty())
+		{
+			arr.pop_back();
+		}
+	}
+	for (int element : arr)
+	{
+		result += element;
+	}
+
+	cout << result;
+	return 0;
+}
+
+
+
