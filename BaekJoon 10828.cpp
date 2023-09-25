d2b6f89d4aa17488b6e33f5b4ed310030438f33c
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-09-25 21:57:33 +0900

    Create BaekJoon 10828.cpp

---
+++ BaekJoon 10828.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    stack<int> stack;
+    cin >> n;
+    if (n < 1 || n > 10000)
+    {
+        return 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        string S; //명령어
+        int X; //push 값
+        cin >> S;
+
+        if (S == "push") 
+        {
+            cin >> X;
+            stack.push(X);
+        }
+
+        else if (S == "pop") 
+        {
+            if (stack.size() == 0)
+            {
+                cout << "-1" << "\n";
+            }
+            else
+            {
+                cout << stack.top() << "\n";
+                stack.pop();
+            }
+        }
+
+        else if (S == "size") 
+        {
+            cout << stack.size() << "\n";
+        }
+
+        else if (S == "empty") 
+        {
+            if (stack.size() == 0)
+            {
+                cout << "1" << "\n";
+            }
+            else
+            {
+                cout << "0" << "\n";
+            }
+        }
+
+        else if (S == "top") 
+        {
+            if (stack.size() == 0)
+            {
+                cout << "-1" << "\n";
+            }
+            else
+            {
+                cout << stack.top() << "\n";
+            }
+        }
+    }
+
+    return 0;
+}
