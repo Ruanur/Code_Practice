25dc9de9b86765127214d82d2cad57af2ad8a5b8
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-10-08 18:41:19 +0900

    Create BaekJoon 11650.cpp
    Utility 라이브러리를 선언하면
    vector에 pair를 사용할 수 있다.
    (2차원 배열)

---
+++ BaekJoon 11650.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm> //sort
+#include <utility> //pair를 사용하기 위함.
+using namespace std;
+
+int main()
+{
+	vector<pair<int, int>> arr;
+	int N;
+	int first;
+	int second;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> first >> second;
+		arr.push_back(make_pair(first,second));
+	}
+
+	sort(arr.begin(), arr.end());
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << arr[i].first << " " << arr[i].second << "\n";
+	}
+}
