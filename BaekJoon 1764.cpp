54f98d7f14f6f90df713005a65bd2fb191812d61
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-10-13 10:57:31 +0900

    Create BaekJoon 1764.cpp
    map 라이브러리 공부

---
+++ BaekJoon 1764.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	int M;
+	string str;
+	map<string, int> map;
+	vector<string> arr;
+	cin >> N >> M;
+	for (int i = 0; i < N + M; i++)
+	{
+		cin >> str;
+		map[str]++;
+	}
+	for (auto entry : map)
+	{
+		if (entry.second > 1)
+		{
+			arr.push_back(entry.first);
+		}
+	}
+	cout << arr.size() << "\n";
+	for (string element : arr)
+	{
+		cout << element << "\n";
+	}
+
+}
