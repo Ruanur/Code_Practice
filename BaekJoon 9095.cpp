fce0cbc237d89b30a77072bf172ecf6e4a53cfec
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-10-27 11:50:49 +0900

    Create BaekJoon 9095.cpp
    - 다이나믹 프로그래밍

---
+++ BaekJoon 9095.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int T;
+	int N;
+	cin >> T;
+	vector <int> arr(12);
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
+	for (int i = 4; i < 12; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+	}
+	for (int i = 1; i <= T; i++)
+	{
+		cin >> N;
+		cout << arr[N] << "\n";
+	}
+		
+}
