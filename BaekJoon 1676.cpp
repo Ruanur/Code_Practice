a668216b0128d33ba267ef0e2ffbb562d10cb014
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-11 06:45:50 +0900

    Create BaekJoon 1676.cpp

---
+++ BaekJoon 1676.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std; 
+
+
+int main()
+{
+	int N;
+	int result = 0;
+	cin >> N;
+	for (int i = 5; i <= N; i *= 5)
+	{
+		result += (N / i);
+	}
+	cout << result;
+
+	return 0;
+}
