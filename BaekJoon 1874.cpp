99652a9bb55f430c90b2bd18e7f041338c36a150
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-13 12:51:23 +0900

    Create BaekJoon 1874.cpp

---
+++ BaekJoon 1874.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	int C = 1;
+
+	vector<int> s(N); //수열 
+	stack<int> stack; //스택
+	vector<char> o; //+,- 
+	for (int i = 0; i < N; i++)
+	{
+		int M;
+		cin >> M;
+		if (!stack.empty() && stack.top() == M)
+		{
+			stack.pop();
+			o.push_back('-');
+		}
+		else if (C <= M)
+		{
+			while (C <= M)
+			{
+				stack.push(C++);
+				o.push_back('+');
+			}
+			stack.pop();
+			o.push_back('-');
+		}
+		else if(!stack.empty() && stack.top() > M)
+		{
+			cout << "NO" << "\n";
+			return 0;
+		}
+	}
+	for (auto N : o)
+	{
+		cout << N << "\n";
+	}
+	return 0;
+}
