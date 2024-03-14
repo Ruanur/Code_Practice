fb29afbc6defa54a48181f8454dab9c11b62cbcf
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-14 10:05:04 +0900

    Create BaekJoon 2751.cpp

---
+++ BaekJoon 2751.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+	int M;
+	vector<int> arr;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> M;
+		arr.push_back(M);
+	}
+
+	sort(arr.begin(), arr.end());
+
+	for (auto N : arr)
+	{
+		cout << N << "\n";
+	}
+	return 0;
+}
