586fbb9692b9d77058abb83b59713e9eb13aa95c
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-15 13:03:05 +0900

    Create BaekJoon 7568.cpp

---
+++ BaekJoon 7568.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> info(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> info[i].first >> info[i].second;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        int rank = 1;
+        for (int j = 0; j < N; j++)
+        {
+            if (i != j && info[i].first < info[j].first && info[i].second < info[j].second)
+            {
+                rank++;
+            }
+        }
+        cout << rank << " ";
+    }
+
+    return 0;
+}
