dee22bee4d3bb93b2e284afc1cb0f8d13f11154f
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-18 20:05:44 +0900

    Create BaekJoon 9375.cpp

---
+++ BaekJoon 9375.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std; 
+
+int main()
+{
+	int N, M;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> M;
+		map<string, int> list;
+		for (int j = 0; j < M; j++)
+		{
+			string dress, wear;
+			cin >> dress >> wear;
+			if (list.find(wear) == list.end())
+			//list에 wear 카테고리가 존재하지 않을 때.
+			{
+				list.insert({ wear, 1 });
+				// {Headgear 1}, 후에 중복 시 int 값 ++
+			}
+			else
+			{
+				list[wear]++;
+				//해당 카테고리 속성 값 증가
+			}
+		}
+		int result = 1;
+		for (const auto& i : list)
+		{
+			result = result * (i.second + 1);
+		}
+		result = result - 1;
+		cout << result << "\n";
+	} 
+	return 0;
+}
