785427ed20fa31fcf5ff49a827c4c31bf936dba2
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-19 22:26:12 +0900

    Create BaekJoon 17626.cpp

---
+++ BaekJoon 17626.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+//동적 계획법
+int main() {
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1; 
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = i;
+        for (int j = 1; j * j <= i; j++)
+        {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+    cout << dp[n];
+    return 0;
+}
