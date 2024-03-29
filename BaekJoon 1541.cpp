9ab9cc6a9922a5d3878c6c8fc443a6576c5a0fed
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-29 18:19:09 +0900

    Update BaekJoon 1541.cpp

--- BaekJoon 1541.cpp
+++ BaekJoon 1541.cpp
@@ -3,31 +3,31 @@
 using namespace std;
 
 int main() {
-    string input;
+    string input; //문자열 입력 
     cin >> input;
 
-    int result = 0;
-    string num;
-    bool isMinus = false;
+    int result = 0; 
+    string num; //숫자를 저장할 문자열 선언 
+    bool isMinus = false; //수식이 '-' 인지
 
-    for (int i = 0; i <= input.size(); i++) {
+    for (int i = 0; i <= input.size(); i++) { //문자열 크기만큼 반복
 
-        if (input[i] == '-' || input[i] == '+' || i == input.size()) {
-            if (isMinus) {
-                result -= stoi(num);
+        if (input[i] == '-' || input[i] == '+' || i == input.size()) { //선택된 문자열이 수식이거나, 제일 마지막 숫자일 경우,
+            if (isMinus) { // '-' 라면
+                result -= stoi(num); //stoi : string to int, 문자열을 숫자로 변환
                 num = "";
             }
-            else {
-                result += stoi(num);
+            else { 
+                result += stoi(num); 
                 num = "";
             }
         }
         else {
-            num += input[i];
+            num += input[i]; //이에 해당하지 않으면(숫자) num 문자열에 저장
         }
 
-        if (input[i] == '-') {
-            isMinus = true;
+        if (input[i] == '-') { //수식이 '-'이면 
+            isMinus = true; //True
         }
     }
 
