8bb136760081cbc48c4ef9981955cc7ca576ffc0
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-03-30 08:40:48 +0900

    Create BaekJoon 9461.cpp

---
+++ BaekJoon 9461.cpp
@@ -0,0 +1,22 @@
+﻿//파도반 수열 
+#include <iostream>
+
+using namespace std; 
+
+long arr[101] = { 0 }; //99를 입력하면 int의 범위를 넘어섬
+
+int main()
+{
+	int N, M;
+	arr[1] = arr[2] = arr[3] = 1;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> M;
+		for (int j = 4; j <= M; j++)
+		{
+			arr[j] = arr[j - 3] + arr[j - 2];
+		}
+		cout << arr[M] << "\n";
+	}
+}
