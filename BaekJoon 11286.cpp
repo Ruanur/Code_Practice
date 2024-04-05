7f50e1c1b445cd1f1071b68352abedc21369c313
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-04-05 15:43:54 +0900

    Create BaekJoon 11286.cpp

---
+++ BaekJoon 11286.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+struct ABS_Compare
+{
+    bool operator()(int a, int b)
+    {
+        if (abs(a) == abs(b))
+        {
+            return a > b;
+        }
+        return abs(a) > abs(b);
+    }
+};
+
+void ABS_Queue(int N)
+{
+    priority_queue<int, vector<int>, ABS_Compare> q;
+    int M;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> M;
+        if (M == 0)
+        {
+            if (q.empty())
+            {
+                cout << "0" << "\n";
+            }
+            else
+            {
+                cout << q.top() << "\n";
+                q.pop();
+            }
+        }
+        else
+        {
+            q.push(M);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N;
+    cin >> N;
+
+    ABS_Queue(N);
+
+    return 0;
+}
