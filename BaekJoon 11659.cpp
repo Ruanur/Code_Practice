17cd8708d3358131196aff79f75581df37c94695
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-04-10 13:16:17 +0900

    Create BaekJoon 11659.cpp

---
+++ BaekJoon 11659.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, M;
+	int num;
+	cin >> N >> M;
+	int* arr = new int[N + 1];
+	arr[0] = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> num;
+		arr[i] = arr[i - 1] + num;
+	}
+	int start, end;
+	for (int i = 0; i < M; i++)
+	{
+		int result = 0;
+		cin >> start >> end;
+		result = arr[end] - arr[start - 1];
+		cout << result << "\n";
+	}
+}
