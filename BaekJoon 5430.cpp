8444b775e40380563c407e4ad02c10498fba34a2
Minkj <91407973+Ruanur@users.noreply.github.com>
2024-10-14 14:06:54 +0900

    BaekJoon 5430
    AC문제, 출력에는 문제가 없는데
    정답이 아님
    
    코드 검토가 필요해보임

---
+++ BaekJoon 5430.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <deque>
+#include <string>
+
+using namespace std;
+
+int T;
+string p;
+int n;
+bool isError = false;
+deque<int> arr; //동적 배열 할당
+
+
+void R(deque<int>& arr) //배열 뒤집기
+{
+	if (!arr.empty())
+	{
+		reverse(arr.begin(), arr.end());
+	}
+}
+
+void D(deque<int>& arr) //첫 번째 배열 지우기
+{
+	if (!arr.empty())
+	{
+		arr.pop_front();
+	}
+	else
+	{
+		cout << "error" << '\n';
+		isError = true;
+		return;
+	}
+}
+
+void result(deque<int>& arr)
+{
+	if (!isError)
+	{
+		cout << '[';
+		while (!arr.empty())
+		{
+			cout << arr.front();
+			arr.pop_front();
+			if (!arr.empty())
+			{
+				cout << ',';
+			}
+		}
+		cout << ']';
+	}
+	arr.clear();
+	isError = false;
+}
+
+int main()
+{
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> p >> n;
+		for (int i = 0; i < n; i++)
+		{
+			int a;
+			cin >> a;
+			arr.push_back(a);
+		}
+
+		for (char command : p)
+		{
+			if (command == 'R')
+			{
+				R(arr);
+			}
+			else if (command == 'D')
+			{
+				D(arr);
+			}
+			else
+			{
+				cout << "잘못된 명령어" << '\n';
+				break;
+			}
+		}
+		result(arr);
+	}
+
+	return 0;
+}
